3fce44fa40adb17f3b2258929d010e5cab8827ac
tpepels <tpepels@693b18f3-7008-e838-0c7e-0c8fdcecac5f>
2011-11-09 20:51:18 +0000

    FastICA implementation converted to Netbeans

---
+++ trunk/FastICA/src/org/fastica/Power3CFunction.java
@@ -0,0 +1,24 @@
+package org.fastica;
+
+
+
+/**
+ * The x^3 function is useful for estimating sub-Gaussian
+ * independent components when there are no outliers.
+ * @author Michael Lambertz
+ */
+
+public class Power3CFunction implements ContrastFunction
+{
+
+public double function( double x )
+{
+	return(x * x * x);
+}
+
+public double derivative( double x )
+{
+	return(3 * x * x);
+}
+
+}
