8b7abcdff46d4225e168eb958df62324a4e62011
m.hermans1989@gmail.com <m.hermans1989@gmail.com@693b18f3-7008-e838-0c7e-0c8fdcecac5f>
2011-12-07 10:03:20 +0000

    Switched some code

--- trunk/ECGSeparation/src/Transforms/CWT.java
+++ trunk/ECGSeparation/src/Transforms/CWT.java
@@ -2,4 +2,35 @@ package Transforms;
 
 public class CWT {
 
+    private static double m;
+    private static double fb;
+    private static double fc;
+    
+    public CWT(double M, double Fb, double Fc) {
+        m = M;
+        fb = Fb;
+        fc = Fc;
+    }
+    
+    public static Complex[] complexTransform(double[] input) {
+        int n = input.length;
+        Complex[] in = new Complex[n];
+        
+        for (int i = 0; i < n; i++) {
+            in[i] = new Complex(input[i], 0); // Complex representation of the input signal.
+        }
+        Complex[] transform = new Complex[n];
+        for (int i = 0; i < n; i++) {
+            double sincPart = fb*i/m;
+            double second = (Math.sin(sincPart*Math.PI)) / (sincPart*Math.PI); //Sinc function
+            second = Math.pow(second, m);
+            
+            Complex first = new Complex(Math.sqrt(fb),0);
+            Complex cSecond = new Complex(second, 0);
+            Complex e = new Complex(0, 2*Math.PI*fc*i);
+            Complex third = e.exp();
+            transform[i] = (first.times(cSecond)).times(third);
+        }
+        return transform;
+    }
 }
