dc7138bfbc5b670879288e3c6a27419db2712acb
m.hermans1989@gmail.com <m.hermans1989@gmail.com@693b18f3-7008-e838-0c7e-0c8fdcecac5f>
2011-12-08 12:59:22 +0000

    Small updates, a start with the layout of what has to be done

--- trunk/ECGSeparation/src/Transforms/ComplexWavelet.java
+++ trunk/ECGSeparation/src/Transforms/ComplexWavelet.java
@@ -2,7 +2,21 @@ package Transforms;
 
 public class ComplexWavelet {
 	
+	private static double[] rawSignal;
+	private static CWT maternalCWT;
+	private static CWT fetalCWT;
+	
 	public static double[][] complexCWT(double[][] input) {
+		// Creating all the Data and methods that need to be used
+		rawSignal = avgSignal(input);
+		maternalCWT = new CWT(3,1,0.5);
+		fetalCWT = new CWT(1,1,0.5);
+		
+		// Maternal ECG calculation
+		CWT.complexTransform(rawSignal);
+		
+		// Fetal ECG calculation
+		CWT.complexTransform(rawSignal);
 		
 	}
 	
